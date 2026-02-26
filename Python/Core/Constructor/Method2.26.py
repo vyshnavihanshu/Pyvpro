3a856961611c7e48e4abeef7ddbb7bb54b301be9
Vyshnavi <degalavyshnavi@gmail.com>
2026-02-26 10:06:52 +0530

    Method Code 2

---
+++ Python/Core/Constructor/Method2.26.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+
+# class Person:
+#     total_person = 0
+#     def __init__(self, name, age,phone):
+#         self.name = name
+#         self.age = age
+#         self.phone = phone
+#
+#         if phone:
+#             person.total_person += 1
+# obj=person("Mani",22,True)
+# obj=person("Rja",21,False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
